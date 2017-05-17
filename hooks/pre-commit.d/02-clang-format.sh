bf3598a3a3da9a62c2471bd518101c8c116c18f5
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-05-17 16:34:06 +0200

    clang-format hook runs only on C++ files.

--- hooks/pre-commit.d/02-clang-format.sh
+++ hooks/pre-commit.d/02-clang-format.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
-#
+#!/bin/bash
+
+#set -x
+set -e
+set -u
+
 # This pre-commit hook checks if any versions of clang-format
 # are installed, and if so, uses the installed version to format
 # the staged changes.
@@ -20,9 +24,9 @@ type "$base" >/dev/null 2>&1 && format="$base"
 # (i.e. clang-format-<$maj_min-$maj_max>-<0-9>)
 if [ -z "$format" ]
 then
-    for j in `seq $maj_max -1 $maj_min`
+    for j in $(seq $maj_max -1 $maj_min)
     do
-        for i in `seq 0 9`
+        for i in $(seq 0 9)
         do
             type "$base-$j.$i" >/dev/null 2>&1 && format="$base-$j.$i" && break
         done
@@ -39,14 +43,19 @@ fi
 
 if git rev-parse --verify HEAD >/dev/null 2>&1
 then
-	against=HEAD
+    against=HEAD
 else
-	# Initial commit: diff against an empty tree object
-	against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+    # Initial commit: diff against an empty tree object
+    against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
 fi
 
 # do the formatting
-for file in `git diff-index --cached --name-only $against`
+for file in $(git diff-index --cached --name-only $against)
 do
-    "$format" -i "$file"
+    if [[ "$file" =~ .*\.(c|h|cpp|hpp|cc)$ ]]; then
+        echo "Formatting $file..."
+        "$format" -i "$file"
+    else
+        echo "Skipping $file..."
+    fi
 done
