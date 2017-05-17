b8185e4c0d1b01f09629a8de27b7e16a2c8750e4
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-05-17 17:05:24 +0200

    Output PACKAGE_STRING if available.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
+#include <stdio.h>
+
 #include "main.h"
+
+#ifdef HAVE_CONFIG_H
+    #include "config.h"
+#endif
 
 int main(void)
 {
+#ifdef HAVE_CONFIG_H
+    printf("PACKAGE_STRING=%s\n", PACKAGE_STRING);
+#else
+    printf("No PACKAGE_STRING available\n");
+#endif
+
     return 0;
 }
 
